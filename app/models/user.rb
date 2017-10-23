99f676de912a841226f82caa9ab12b257d71d39c
Fuyuki <ocs.suzuki.f@gmail.com>
2017-10-23 11:58:40 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ApplicationRecord
 
 # アカウントを有効にする
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
