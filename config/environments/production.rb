e9f61ebaa9e77bdd089214c4e51233b551dbdd50
Fuyuki <ocs.suzuki.f@gmail.com>
2017-10-26 09:58:20 +0900

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,12 +69,12 @@ Rails.application.configure do
   host = 'monoloog.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'ocs.suzuki.f',
-    :password       => 'Lalalika0',
-    :domain         => 'smtp.gmail.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
   
