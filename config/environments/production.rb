fa301880d4f4dfd64ca3a942a3d8867cd63187ef
unknown <164128@lab-ocs.local>
2017-10-27 11:16:08 +0900

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'monoloog.herokuapp.com'
+  host = 'monolog.site'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => 'ocs.suzuki.f',
+    :password       => 'READchip0',
+    :domain         => 'smtp.gmail.com',
     :enable_starttls_auto => true
   }
   
